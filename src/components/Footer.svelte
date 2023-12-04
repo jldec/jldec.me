3d79e25102a191bdfd6456de5e76107dbd6c93e4
jldec <jldec@users.noreply.github.com>
2023-12-04 13:53:56 +0000

    highlight go, tweak footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,4 @@
-<div class="font-display text-xs uppercase p-4 text-slate-400">
-  Built with ❤️ using 💥 <a href="https://github.com/zeroasiccorp/zaui" class="underline">zaui</a>
+<div class="text-sm p-4 text-slate-400">
+  Built with ❤️ using 💥 <a href="https://github.com/zeroasiccorp/zaui" class="underline">zaui</a>.<br>
+  Hosted on <a href="https://pages.cloudflare.com/" class="underline">Cloudflare pages</a>.<br>
 </div>
