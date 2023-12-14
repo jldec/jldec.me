d9fe1093322e3e995d0f829e147c27914d6f84aa
jldec <jldec@users.noreply.github.com>
2023-12-14 10:00:07 +0000

    float right

--- src/components/Mono.svelte
+++ src/components/Mono.svelte
@@ -6,13 +6,14 @@
   export let content: MarkdownFile;
 </script>
 
-<div class="relative pr-[110px] prose-headings:my-2 prose-headings:font-mono prose-headings:text-green-700 prose-headings:px-4 prose-headings:-mx-4">
-  <img
-    src={fixlink(`jldec.jpg`)}
-    alt="jldec"
-    class="absolute right-0 top-0 w-[100px] h-[100px] m-0 object-cover rounded-full"
-    width="100"
-    height="100"
-  />
+<img
+  src={fixlink(`jldec.jpg`)}
+  alt="jldec"
+  class="float-right w-[100px] h-[100px] mx-2 object-cover rounded-full"
+  width="100"
+  height="100"
+/>
+
+<div class="prose-headings:font-mono prose-headings:text-green-700 prose-headings:px-4 prose-headings:-mx-4 prose-headings:my-2">
   <Markdoc {content} />
 </div>
